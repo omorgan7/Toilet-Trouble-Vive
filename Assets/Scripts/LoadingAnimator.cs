0cebe4ac9d48bed1ccdd678a42f77270ca12262e
Owen Morgan <o.morgan@bath.ac.uk>
2017-09-08 16:35:46 +0100

    Added menu and loading screen system.

---
+++ Assets/Scripts/LoadingAnimator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingAnimator : MonoBehaviour {
+	Image image;
+	public float asyncLevelProgress = 0f;
+	public bool isDone = false;
+	float prevLevelProgress = 0f;
+
+	void Start(){
+		image = gameObject.GetComponent<Image>();
+	}
+
+	public void FillAnimate(float t){
+		t = Mathf.Clamp(t,0,1);
+		image.fillAmount = (1-t);
+	}
+
+	void LateUpdate(){
+		if(prevLevelProgress >= 1f){
+			isDone = true;
+			return;
+		}
+		if(prevLevelProgress < asyncLevelProgress){
+			prevLevelProgress += Time.deltaTime;
+			FillAnimate(prevLevelProgress);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	SceneState scenestate;
+	AsyncOperation ao;
+	public LoadingAnimator animator;
+
+	void Start(){
+		var temp = GameObject.Find("SceneStatePreserver");
+		if(temp == null){
+			return;
+		}
+		scenestate = temp.GetComponent<SceneState>();
+		StartCoroutine(LoadNextLevel());
+	}
+
+	IEnumerator LoadNextLevel(){
+		ao = SceneManager.LoadSceneAsync(scenestate.SceneIndex);
+		ao.allowSceneActivation = false;
+		
+		while(ao.progress <= 0.9 && !animator.isDone){
+			animator.asyncLevelProgress = ao.progress + 0.1f;
+			yield return null;
+		}
+
+		ao.allowSceneActivation = true;
+		yield return true;
+	}
+	
+}
