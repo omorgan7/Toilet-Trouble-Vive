db471ad300eccb60f475b5cf13cddf9b75c76ee2
Catherine <cat.taylor.taylor@btinternet.com>
2017-09-11 16:03:31 +0100

    Added in rope.

---
+++ Assets/Scripts/toiletPosition.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toiletPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(gameObject.transform.position.y < 0f){
+			print("below zero");
+			gameObject.transform.GetComponent<Rigidbody>().AddForce(0f,1f,0f);
+		}
+		
+	}
+}
