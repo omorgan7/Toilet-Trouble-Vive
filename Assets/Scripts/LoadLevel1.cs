4fbf596f0b1082ad040a858ca58bac710b9689d1
Catherine <cat.taylor.taylor@btinternet.com>
2018-03-29 12:59:10 +0100

    Created script to initiate level 1

---
+++ Assets/Scripts/LoadLevel1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadLevel1 : MonoBehaviour {
+	private GameObject toilet;
+	private GameObject door;
+	private GameObject pictureFrame;
+
+	private GameObject bathroom;
+	private GameObject Light;
+	
+
+	// Use this for initialization
+	void Start () {
+
+		toilet = Resources.Load("Level 1/toilet") as GameObject;
+		Instantiate(toilet, new Vector3(0.004f, -0.8f, 0.75f), Quaternion.identity);
+
+		door = Resources.Load("Level 1/door") as GameObject;
+		door.transform.localScale = new Vector3(0.16f, 0.12f, 0.18f);
+		Instantiate(door, new Vector3(0.0f, -0.4f, -1.1f), Quaternion.Euler(180, 90, 90));
+
+		pictureFrame = Resources.Load("Level 1/pictureFrame") as GameObject;
+		pictureFrame.transform.localScale = new Vector3(0.07f, 0.06f, 0.07f);
+		Instantiate(pictureFrame, new Vector3(-0.25f, 0.25f, 1.3f), Quaternion.Euler(90, 180, 0));
+
+		bathroom = Resources.Load("Level 1/bathroom") as GameObject;
+		bathroom.transform.localScale = new Vector3(1.2f, 1,1);
+		Instantiate(bathroom, new Vector3(0, 0, 0), Quaternion.Euler(-90, 0, 90));
+
+		Light = Resources.Load("Level 1/Light") as GameObject;
+		Light.transform.localScale = new Vector3(0.2f, 1, 0.2f);
+		Instantiate(Light, new Vector3(0, 0.8f, 0.75f), Quaternion.Euler(0, 0, -90));
+		Instantiate(Light, new Vector3(0, 0.8f, -0.75f),  Quaternion.Euler(0, 0, -90));
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/ObjectRespawner_noWalls.cs
+++ Assets/Scripts/ObjectRespawner_noWalls.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectRespawner : MonoBehaviour {
+public class ObjectRespawner_noWalls : MonoBehaviour {
 	public GameObject player;
 	public void OnTriggerExit(Collider other){
 		if(other.isTrigger){
