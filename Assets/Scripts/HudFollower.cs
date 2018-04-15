13527a535ba4442eb6634dda9af402c855161ab6
Owen Morgan <omorgan7@me.com>
2018-04-15 16:08:19 +0100

    Added a small script that makes the hud follow a gameobject's position.

---
+++ Assets/Scripts/HudFollower.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class HudFollower : MonoBehaviour {
+
+	public GameObject hud;
+	public GameObject toFollow;
+	
+	// Update is called once per frame
+	void Update () {
+		hud.transform.position = toFollow.transform.position;
+	}
+}
